e6ddaa62b93cb35880b517766d90bcc560aec4e3
Scott Linderman <scott.linderman@gmail.com>
2015-02-04 19:54:42 -0500

    adding openmp to setup script

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy as np
 
+extra_compile_args = ['-fopenmp']
+extra_link_args    = ['-fopenmp']
+
+ext_modules = cythonize('**/*.pyx')
+for e in ext_modules:
+    e.extra_compile_args.extend(extra_compile_args)
+    e.extra_link_args.extend(extra_link_args)
+
 setup(name='pyhawkes',
       version='0.1',
       description='Bayesian inference for network Hawkes processes',
       author='Scott Linderman',
       author_email='slinderman@seas.harvard.edu',
       url='http://www.github.com/slinderman/pyhawkes',
-      ext_modules=cythonize('**/*.pyx'),
+      ext_modules=ext_modules,
       include_dirs=[np.get_include(),],
      )
